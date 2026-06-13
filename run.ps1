767451d159f31c4eff69c875ad0de894b36652d9
Abdul Rehman <abdulrehman539eb.dev@gmail.com>
2026-06-13 10:00:31 +0500

    Add files via upload

---
+++ run.ps1
@@ -0,0 +1,6 @@
+﻿cd C:\projects\predictx_app\android
+.\gradlew assembleDebug
+New-Item -ItemType Directory -Force -Path "C:\projects\predictx_app\build\app\outputs\flutter-apk" | Out-Null
+Copy-Item -Force "C:\projects\predictx_app\android\app\build\outputs\flutter-apk\app-debug.apk" "C:\projects\predictx_app\build\app\outputs\flutter-apk\app-debug.apk"
+cd C:\projects\predictx_app
+flutter run --no-build
